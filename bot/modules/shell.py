1643c6775bbdd78d8db52e0cc1dd4510ce48d01b
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-07-25 23:14:56 +0530

    Update shell.py

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -19,10 +19,10 @@ async def shell(_, message):
     stdout, stderr, _ = await cmd_exec(cmd, shell=True)
     reply = ""
     if len(stdout) != 0:
-        reply += f"*Stdout*\n<code>{stdout}</code>\n"
+        reply += f"<b>☞ BASH\n\n• COMMAND\n<code>{cmd}</code>• OUTPUT\n ≤code>{stdout}</code>\n"
         LOGGER.info(f"Shell - {cmd} - {stdout}")
     if len(stderr) != 0:
-        reply += f"*Stderr*\n<code>{stderr}</code>"
+        reply += f"<b>☞ BASH\n\n• COMMAND\n<code>{cmd}</code>• OUTPUT\n ≤code>{stderr}</code>\n"
         LOGGER.error(f"Shell - {cmd} - {stderr}")
     if len(reply) > 3000:
         with BytesIO(str.encode(reply)) as out_file:
