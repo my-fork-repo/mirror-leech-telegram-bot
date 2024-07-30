0ff3f943d2f6df68d4b5621a8cd424ebff21d6d7
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-07-30 09:57:35 +0530

    Update authorize.py

--- bot/modules/authorize.py
+++ bot/modules/authorize.py
@@ -84,7 +84,7 @@ async def removeSudo(client, message):
 
 bot.add_handler(
     MessageHandler(
-        authorize, filters=command(BotCommands.AuthorizeCommand) & CustomFilters.message.chat.id
+        authorize, filters=command(BotCommands.AuthorizeCommand) & CustomFilters.user.id
     )
 )
 bot.add_handler(
