b49d40de393cca06e34ed6b113f81a5d55d1fcee
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-08-11 16:56:39 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,3 +1,25 @@
+import asyncio
+from pyrogram import Client, filters
+from datetime import datetime
+
+from bot import bot
+
+@bot.on_message(filters.new_chat_members)
+async def welcome(client, message):
+    for new_member in message.new_chat_members:
+        member_name = new_member.first_name if new_member.first_name else "there"
+        user_name = f"@{new_member.username}" if new_member.username else ""
+        user_id = new_member.id
+        now = datetime.now()
+        join_date = now.strftime("%Y-%m-%d")
+        join_time = now.strftime("%I:%M:%S %p")
+        chat_members_count = await client.get_chat_members_count(message.chat.id)
+        msg = f"Hlw {member_name} {user_name}\n"
+        msg += f"Your ID: <code>{user_id}</code>\n"
+        msg += f"Join date: {join_date}\n"
+        msg += f"Join time: {join_time}\n"
+        msg += f"Member number: {chat_members_count}"
+        
 from aiofiles import open as aiopen
 from aiofiles.os import path as aiopath, remove
 from asyncio import gather, create_subprocess_exec
@@ -159,6 +181,7 @@ async def ping(_, message):
     end_time = int(round(time() * 1000))
     await editMessage(reply, f"{end_time - start_time} ms",f"<b>\nBot Uptime:</b> {get_readable_time(time() - botStartTime)}\n")
     await sendMessage(message,f"<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n")
+    await message.reply_text(msg)
 
 
 async def log(_, message):
