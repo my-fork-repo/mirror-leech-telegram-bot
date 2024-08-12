81069dc99c1dd7ec2da41e8fb5e40b6fdc93b850
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-08-12 17:36:10 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -188,7 +188,7 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
                 else task.progress()
             )
             msg += f"\n{get_progress_bar_string(progress)} {progress}"
-            msg += f"\n<b>Processed:</b> {task.processed_bytes()} of {task.size()}"
+            msg += f"\n<b>Processed:</b> {task.processed_bytes()} of {task.size()} "
             msg += f"\n<b>Speed:</b> {task.speed()} | <b>ETA:</b> {task.eta()}"
             if hasattr(task, "seeders_num"):
                 try:
@@ -203,7 +203,7 @@ async def get_readable_message(sid, is_user, page_no=1, status="All", page_step=
             msg += f" | <b>Time: </b>{task.seeding_time()}"
         else:
             msg += f"\n<b>Size: </b>{task.size()} "
-        msg += f"\n<b>Gid: </b>/c {task.gid()}\n\n"
+        msg += f"\n<b>Gid: </b><code>/c {task.gid()}</code>\n\n"
 
     if len(msg) == 0:
         if status == "All":
