c6c85325748f7ee04db63d4d8af985abefbfe12c
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2024-11-14 18:54:06 +0530

    Update ul.py

--- bot/modules/ul.py
+++ bot/modules/ul.py
@@ -1,6 +1,7 @@
 from bot.helper.telegram_helper.message_utils import send_file, send_message
-cmd = message.text.split(maxsplit=1)
-if len(cmd) == 1:
-  await send_message(message, "<code>No command to execute was given.</code>")
-  return
-await send_file(message, f"{cmd}")
+assync def ul(_,message):
+  cmd = message.text.split(maxsplit=1)
+  if len(cmd) == 1:
+    await send_message(message, "<code>No command to execute was given.</code>")
+    return
+  await send_file(message, f"{cmd}")
